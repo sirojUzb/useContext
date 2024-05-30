9eb6af796704fb76c7e0382a83a452b15573dc42
sirojUz <sirojiddinmamatovuzb@gmail.com>
2024-05-31 04:32:00 +0900

    useCon

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -1,6 +1,6 @@
 const index = () => {
   return (
-    <div className="bg-green-300 flex items-center justify-center pl-[30px] pr-[30px] h-[80px]">
+    <div className="bg-green-300 flex items-center justify-center pl-[30px] pr-[30px] h-[80px] cursor-pointer">
       <h3>&copy; 2024</h3>
     </div>
   );
--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -1,8 +1,8 @@
 const index = () => {
   return (
     <div className="bg-green-300 flex items-center justify-between pl-[30px] pr-[30px] h-[80px]">
-      <h3>O{"'"}zbek kino</h3>
-      <h3>Kino soni: 11</h3>
+      <h3 className="cursor-pointer">O{"'"}zbek kino</h3>
+      <h3 className="cursor-pointer">Kino soni: 11</h3>
     </div>
   );
 };
